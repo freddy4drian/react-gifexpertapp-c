6f342f2d7b021ba8edae7cb0e4005dd048e0979c
Freddy Adrián <62298083+freddy4drian@users.noreply.github.com>
2021-06-23 22:22:23 -0400

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+
+export const getGifs = async( category ) => {
+
+    const api_key = 'MfUpLfhZP3fqbkdU7vKgZ1hUTvW8LI5g';
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${api_key}`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+    
+}
